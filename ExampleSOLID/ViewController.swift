b23f6482b00ceab58caca9d28a1e31f8d909f7bf
Alexey Anisimov <vkrotin@gmail.cpm>
2021-01-29 12:08:18 +0500

    Initial Commit

---
+++ ExampleSOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleSOLID
+//
+//  Created by  vkrotin on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
